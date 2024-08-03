e064eee622d5fc0f0185fc73595b65d49c901420
Xeth-0 <tediberhanu777@gmail.com>
2024-08-03 17:30:15 +0300

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -22,9 +22,20 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
         return false;
     }
     else if (pattern[0] == '[' && pattern[pattern.length() - 1] == ']') {
-        const std::string string_to_match = pattern.substr(1, pattern.length() - 2); // the length of the pattern we are matching([___]) is pattern.length() - 2 (minus the "[]")
-        std::cout << string_to_match << std::endl;
-        return input_line.find_first_of(string_to_match) != std::string::npos;
+        int negative_char_group = pattern[1] == '^'; // Check for negative char group, which has the pattern [^___] instead of [___]
+
+        const std::string string_to_match = pattern.substr(1 + negative_char_group, pattern.length() - 2 - negative_char_group);
+        // the length of the pattern we are matching([___]) is pattern.length() - 2 (minus the "[]" and "^"(if exists))
+       // String to match could just be moved to the conditional below.... but yeah i'm kinda stubborn huh.
+
+        if (negative_char_group == 0) {
+            // Positive char group
+            return input_line.find_first_of(string_to_match) != std::string::npos;
+        }
+        else {
+            // Negative char group
+            return input_line.find_first_not_of(string_to_match) != std::string::npos;
+        }
     }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
