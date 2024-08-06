92f0ae0272d5c7e9595eefb70e4e3d7aa4602972
Xeth-0 <tediberhanu777@gmail.com>
2024-08-06 18:29:05 +0300

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,58 +1,19 @@
 #include <iostream>
 #include <string>
 
-// bool match_pattern(const std::string& input_line, const std::string& pattern) {
-//     if (pattern.length() == 1) {   // Single letter(number) search. Not regex as that would require more than one character
-//         std::cout << pattern << std::endl;
-//         return input_line.find(pattern) != std::string::npos;
-//     }
-//     else if (pattern == "\\d") {
-//         // find_first_of() matches any of the characters specified in the args. This way
-//         // we are checking for any numbers within the input line.
-//         // It returns string::npos if it doesn't find any. It is used to indicate no matches.
-//         return input_line.find_first_of("0123456789") != std::string::npos;  // 1 if match, 0 if none.
-//     }
-//     else if (pattern == "\\w") {
-//         // return input_line.find_first_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") != std::string::npos;  // 1 if match, 0 if none.
-//         for (const char& l : input_line) {
-//             if (std::isdigit(l) || std::isalpha(l)) {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-//     else if (pattern[0] == '[' && pattern[pattern.length() - 1] == ']') {
-//         int negative_char_group = pattern[1] == '^'; // Check for negative char group, which has the pattern [^___] instead of [___]
-
-//         const std::string string_to_match = pattern.substr(1 + negative_char_group, pattern.length() - 2 - negative_char_group);
-//         // the length of the pattern we are matching([___]) is pattern.length() - 2 (minus the "[]" and "^"(if exists))..... yes this could've been simpler
-//         // String to match could just be moved to the conditional below.... but yeah i'm kinda stubborn huh.
-
-//         if (negative_char_group) // Positive char group
-//             return input_line.find_first_of(string_to_match) != std::string::npos;
-//         else // Negative char group
-//             return input_line.find_first_not_of(string_to_match) != std::string::npos;
-//     }
-//     else {
-//         throw std::runtime_error("Unhandled pattern " + pattern);
-//     }
-// }
-
 bool match_symbol(const char* text, const char* pattern);
-bool match_character_group(const char* text, const char* pattern, bool negative_char_group);
-// bool match_negative_character_group(const char* text, const char* pattern);
 bool match_pattern(const std::string& input_line, const std::string& pattern);
+bool match_character_group(const char* text, const char* pattern, bool negative_char_group);
 
 
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     // Starting point for the matcher. Handles any special patterns like character groups itself. Otherwise calls match_symbol.
-
     const char* text_ptr = input_line.c_str(); // Pointer to iterate over the input_line. (Wanted to avoid creating a new copy)
     const char* pattern_ptr = pattern.c_str(); // Pointer to iterate over the pattern.
 
     // Special matches
+    // Character Groups
     if (pattern_ptr[0] == '[') {
-        // No clue how to do this one yet.
         if (pattern_ptr[1] != '^' && match_character_group(text_ptr, pattern_ptr + 1, false)) // positive character group
             return 1;
         if (pattern_ptr[1] == '^' && match_character_group(text_ptr, pattern_ptr + 1, true))  // negative character group
@@ -60,7 +21,10 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
 
         return 0;   // Failed to match.
     }
-
+    // Start of String Anchor
+    if (pattern_ptr[0] == '^') {
+        return match_symbol(text_ptr, pattern_ptr + 1); // Avoids the loop below, so only matches at the start.
+    }
     // Iterate over the text and attempt to match the pattern.
     do {
         if (match_symbol(text_ptr, pattern_ptr)) return 1;
